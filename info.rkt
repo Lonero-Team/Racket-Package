1f05e9856fab6e0692ca3eb09735e91ebf74d0ec
Andrew Kamal <kamalandrew55@gmail.com>
2020-04-08 06:21:07 +0000

    add files

---
+++ info.rkt
@@ -0,0 +1,9 @@
+#lang info
+(define collection "decentralized-internet")
+(define deps '("base"
+               "rackunit-lib"))
+(define build-deps '("scribble-lib" "racket-doc"))
+(define scribblings '(("scribblings/decentralized-internet.scrbl" ())))
+(define pkg-desc "A package for building distributed computing projects")
+(define version "0.0")
+(define pkg-authors '(gamer456148))
